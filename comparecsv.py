c6451a3113ea4933f43fd21461145084bb387523
TanuNair <64299906+TanuNair@users.noreply.github.com>
2023-09-14 18:21:05 +0530

    Update comparecsv.py
    Compare two csv files and write a report

--- comparecsv.py
+++ comparecsv.py
@@ -9,15 +9,15 @@ def read_csv(file_path):
             data.append(row)
     return data
 
-file1_data = read_csv("/Users/tanunair/Documents/TPS deployment/Prod TQM.csv")
-file2_data = read_csv("/Users/tanunair/Documents/TPS deployment/SAT TQM.csv")
+file1_data = read_csv("Prod-data.csv")
+file2_data = read_csv("SAT-data.csv")
 
 def compare_rows(row1, row2):
     return row1 == row2
 
 mismatched_rows = []
 
-with open('/Users/tanunair/Documents/TPS deployment/diff_report_TQM.txt', 'w') as report_file:
+with open('diff_report.txt', 'w') as report_file:
     for i, (row1, row2) in enumerate(zip(file1_data, file2_data), start=1):
         if compare_rows(row1, row2):
             report_file.write(f'Row {i} matches\n')
@@ -26,7 +26,7 @@ with open('/Users/tanunair/Documents/TPS deployment/diff_report_TQM.txt', 'w') a
             mismatched_rows.append((row1, row2))
 
 # Write mismatched rows from both files
-with open('/Users/tanunair/Documents/TPS deployment/diff_report_TQM.txt', 'a') as report_file:
+with open('diff_report.txt', 'a') as report_file:
     for i, (row1, row2) in enumerate(mismatched_rows, start=1):
       report_file.write(f'\nMismatched columns for Row {i}:\n')
       report_file.write(f'File 1 PROD: {",".join(row1)}\n')
